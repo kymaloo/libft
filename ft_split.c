419466e8c524f1d484c0a813ec48d818453d7ca0
kymaloo <91215909+kymaloo@users.noreply.github.com>
2023-11-10 15:03:33 +0100

    Add files via upload

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: trgaspar <trgaspar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 10:05:59 by trgaspar          #+#    #+#             */
-/*   Updated: 2023/11/08 18:56:13 by trgaspar         ###   ########.fr       */
+/*   Updated: 2023/11/10 14:58:43 by trgaspar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,31 +37,31 @@ static char	**ft_free_all(char **tab, int i)
 	return (NULL);
 }
 
-char	**ft_split(char const *s, char c)
+char	**ft_split(const char *s, char c)
 {
-	int		i_len_nbword[3];
-	char	**tab;
+	int			nbword;
+	char		**tab;
+	const char	*start;
+	int			i;
 
+	i = 0;
 	if (!s)
 		return (NULL);
-	i_len_nbword[0] = 0;
-	i_len_nbword[2] = ft_count_word(s, c);
-	tab = malloc(sizeof(char *) * (i_len_nbword[2] + 1));
+	nbword = ft_count_word(s, c);
+	tab = (char **)malloc(sizeof(char *) * (nbword + 1));
 	if (!tab)
 		return (NULL);
-	while (i_len_nbword[0] < i_len_nbword[2])
+	while (i < nbword)
 	{
 		while (*s && *s == c)
 			s++;
-		if (ft_strchr(s, c))
-			i_len_nbword[1] = ft_strchr(s, c) - s;
-		else
-			i_len_nbword[1] = ft_strlen(s);
-		tab[i_len_nbword[0]] = ft_substr(s, 0, i_len_nbword[1]);
-		if (!tab[i_len_nbword[0]++])
-			return (ft_free_all(tab, i_len_nbword[0] - 1));
-		s += i_len_nbword[1];
+		start = s;
+		while (*s && *s != c)
+			s++;
+		tab[i] = ft_substr(start, 0, s - start);
+		if (!tab[i++])
+			return (ft_free_all(tab, i - 1));
 	}
-	tab[i_len_nbword[0]] = 0;
+	tab[i] = NULL;
 	return (tab);
 }
