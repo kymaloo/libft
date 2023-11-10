4f5ee28156e3eb96a5e4eb949e2b3a56e2720b39
kymaloo <91215909+kymaloo@users.noreply.github.com>
2023-11-10 02:41:22 +0100

    Add files via upload

---
+++ ft_splits.c
@@ -0,0 +1,54 @@
+#include "libft.h"
+
+static int	ft_count_word(char const *str, char c)
+{
+	int	i;
+
+	i = 0;
+	while (*str)
+	{
+		while (*str == c)
+			str++;
+		if (*str != c && *str)
+			i++;
+		while (*str != c && *str)
+			str++;
+	}
+	return (i);
+}
+
+static void	ft_free_all(char **tab, int i)
+{
+	while (i >= 0)
+		free(tab[i--]);
+	free(tab);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	int		nbword;
+	char	**tab;
+    const char *start;
+    int i;
+
+	i = 0;
+    if (!s)
+		return (NULL);
+	nbword = ft_count_word(s, c);
+	**tab = (char **)malloc(sizeof(char *) * (nbword + 1));
+	if (!tab)
+		return (NULL);
+	while (i < nombre_mots)
+	{
+		while (*s && *s == c)
+			s++;
+		start = s;
+		while (*s && *s != c)
+			s++;
+		tab[i] = ft_substr(start, 0, s - start);
+		if (!tab[i++])
+			return (ft_free_all(tab, i - 1));
+	}
+	tab[i] = NULL;
+	return (tab);
+}
